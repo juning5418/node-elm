3ec39bab706774ecbb552a8cc174f3caeefeacf2
juning <691092674@qq.com>
2018-06-19 12:48:09 +0800

    添加 商品详情接口

--- mongodb/db.js
+++ mongodb/db.js
@@ -10,20 +10,20 @@ const db = mongoose.connection;
 
 db.once('open' ,() => {
 	console.log(
-    chalk.green('连接数据库成功')
+    //chalkgreen('连接数据库成功')
   );
 })
 
 db.on('error', function(error) {
     console.error(
-      chalk.red('Error in MongoDb connection: ' + error)
+      //chalkred('Error in MongoDb connection: ' + error)
     );
     mongoose.disconnect();
 });
 
 db.on('close', function() {
     console.log(
-      chalk.red('数据库断开，重新连接数据库')
+      //chalkred('数据库断开，重新连接数据库')
     );
     mongoose.connect(config.url, {server:{auto_reconnect:true}});
 });
